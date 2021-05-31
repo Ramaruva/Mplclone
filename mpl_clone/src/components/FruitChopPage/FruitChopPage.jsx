c5bfd62df46df9db6e16ed1fc5bff83800a65e17
Bhargav1224 <bhargavkudala@gmail.com>
2021-05-31 10:23:40 +0530

    updated home page

--- mpl_clone/src/components/FruitChopPage/FruitChopPage.jsx
+++ mpl_clone/src/components/FruitChopPage/FruitChopPage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export const FruitChopPage = () => {
     return (
         <div>
-            
+            <h1></h1>
         </div>
     )
 }
--- mpl_clone/src/routes/Routes.jsx
+++ mpl_clone/src/routes/Routes.jsx
@@ -2,6 +2,8 @@ export const Routes =()=>
 {
     return(
         <>
+            
+            
         </>
     )
 }
